2e003ccba2d530800bd6f1ec9328c581ca25462d
David Barratt <davidwbarratt@411675.no-reply.drupal.org>
2014-07-19 12:50:35 -0400

    Refresh the child rows onDrop. Fixes #2294397

--- js/insert_field.field_ui.js
+++ js/insert_field.field_ui.js
@@ -27,7 +27,13 @@ Drupal.behaviors.insertFieldFieldUI = {
       // Get the ID of the table.
       var id = jQuery(table).attr('id');
 
-      // Save the existing validIndentInterval() method for later use.
+      // Save the existing onDrop() method for later use.
+      Drupal.tableDrag[id]._onDrop = Drupal.tableDrag[id].onDrop;
+
+      // Override the existing onDrop() method.
+      Drupal.tableDrag[id].onDrop = Drupal.insertFieldFieldUI.onDrop;
+
+      // Save the existing validIndentInterval() for later use.
       Drupal.tableDrag[id].row.prototype._validIndentInterval = Drupal.tableDrag[id].row.prototype.validIndentInterval;
 
       // Override the existing validIndentInterval() method.
@@ -40,8 +46,133 @@ Drupal.behaviors.insertFieldFieldUI = {
 
 Drupal.insertFieldFieldUI = {};
 
+/*
+ * Replaces the Field UI onDrop() method.
+ * This is done to keep the 'region' up to date with the field children.
+ *
+ * @see: Drupal.fieldUIOverview.onDrop().
+ */
+Drupal.insertFieldFieldUI.onDrop = function() {
+  var dragObject = this;
+  var row = dragObject.rowObject.element;
+  var rowHandler = jQuery(row).data('fieldUIRowHandler');
+
+  // Get the Insert Field Settings.
+  var settings = Drupal.settings.insert_field;
+
+  // Get the Formatter Value.
+  var value = jQuery('.field-formatter-type', row).get(0).value;
+
+  // Start an empty refresh object for storing all of the refreshRows.
+  // This allows use to perform a single AJAXRefreshRows().
+  var refresh = {};
+
+  // Start the refreshRows as an empty object.
+  var refreshRows = {};
+
+  // Start the Region as an empty string since the region we are moving towards
+  // is not yet known.
+  var region = '';
+
+  // Ensure that the rowHandler is defined.
+  if (typeof rowHandler != 'undefined') {
+
+    // Get the Region we are moving towards.
+    var regionRow = jQuery(row).prevAll('tr.region-message').get(0);
+    var region = regionRow.className.replace(/([^ ]+[ ]+)*region-([^ ]+)-message([ ]+[^ ]+)*/, '$2');
+
+    // Attempt to refresh the row.
+    if (refreshRows = Drupal.insertFieldFieldUI.regionChange(region, rowHandler)) {
+
+      // Add the refresh row to our mastor object.
+      refresh = jQuery.extend(refreshRows, refresh);
+
+    }
+
+  }
+
+  // If a Region was found, and the row is an insert field:
+  if (region && typeof settings.row_id[row.id] != 'undefined') {
+
+    // Loop through every parent field, searching for children to the current row.
+    jQuery('select.field-parent').each(function() {
+
+      // Get the value and compare it to the current row field id.
+      if (jQuery(this).get(0).value == settings.row_id[row.id]) {
+
+        // Get the Row.
+        var childRow = jQuery(this).parents('tr');
+
+        // Get the rowHandler for the current child row.
+        var childRowHandler = jQuery(childRow).data('fieldUIRowHandler');
+
+        // Ensure that the rowHandler does in fact exist.
+        if (typeof childRowHandler != 'undefined') {
+
+          // Attempt to refresh the child row(s).
+          if (refreshRows = Drupal.insertFieldFieldUI.regionChange(region, childRowHandler)) {
+
+            // Add the refresh row to our mastor object.
+            refresh = jQuery.extend(refreshRows, refresh);
+
+          }
+
+        }
+
+
+
+      }
+
+    })
+
+  }
+
+  // Ajax-update the rows.
+  Drupal.fieldUIOverview.AJAXRefreshRows(refreshRows);
+
+}
+
+/*
+ * Region change method.
+ *
+ * @see: Drupal.fieldUIOverview.onDrop().
+ */
+Drupal.insertFieldFieldUI.regionChange = function(region, rowHandler) {
+
+  // Check to ensure that the region is actually changing.
+  if (region != rowHandler.region) {
+
+    // Let the row handler deal with the region change.
+    refreshRows = rowHandler.regionChange(region);
+
+    // Update the row region.
+    rowHandler.region = region;
+
+    // Return the refreshRows object.
+    return refreshRows;
+
+  }
+
+  // Since nothing was done, return an empty object.
+  return {};
+
+}
+
 Drupal.insertFieldFieldUI.row = {};
 
+/*
+ * Save the existing validIndentInterval() method for later use.
+ */
+Drupal.insertFieldFieldUI.row._validIndentInterval = Drupal.tableDrag.prototype.row.prototype.validIndentInterval;
+
+/*
+ * Override the exisitng validIndentInterval() method.
+ * Since some fields can be inserted (under certain condtions), and some cannot
+ * be inserted at all, we must instruct the rowHandler if the indentation is
+ * valid or not.
+ *
+ * @see: Drupal.tableDrag.prototype.row.prototype.validIndentInterval.
+ */
 Drupal.insertFieldFieldUI.row.validIndentInterval = function (prevRow, nextRow) {
 
   // Get the Current Row.
@@ -51,7 +182,7 @@ Drupal.insertFieldFieldUI.row.validIndentInterval = function (prevRow, nextRow) 
   var settings = Drupal.settings.insert_field;
 
   // Execute the Existing validIndentInterval() method.
-  var interval = Drupal.tableDrag.prototype.row.prototype._validIndentInterval(prevRow, nextRow);
+  var interval = Drupal.insertFieldFieldUI.row._validIndentInterval(prevRow, nextRow);
 
   // If the Interval Max is not 0:
   if (interval.max) {
