cdddb7f6fb6d52df35887d81f9469607abecc8b4
David Barratt <davidwbarratt@411675.no-reply.drupal.org>
2014-08-02 16:23:31 -0400

    Issue #2310279 by davidwbarratt: Fixed Field Config Javascrpt.

--- modules/insert_field_ui/js/insert_field_ui.field_ui.js
+++ modules/insert_field_ui/js/insert_field_ui.field_ui.js
@@ -14,13 +14,13 @@ Drupal.behaviors.insertFieldUIFieldUI = {
       insert_field.row_id = new Object();
 
       // Loop through the Field Settings.
-      for (var field in Drupal.settings.insert_field.field_settings) {
+      for (var field in Drupal.settings.insert_field.fields) {
 
         // Save the field_machine_name indexed by row id.
         insert_field.row_id[field.replace('_', '-')] = field;
 
         // Save the row_id indexed by field_machine_mane.
-        insert_field.field_settings[field].row_id = field.replace('_', '-');
+        insert_field.fields[field].row_id = field.replace('_', '-');
 
       }
 
@@ -191,111 +191,124 @@ Drupal.insertFieldFieldUI.row.validIndentInterval = function (prevRow, nextRow) 
   // Execute the Existing validIndentInterval() method.
   var interval = Drupal.insertFieldFieldUI.row._validIndentInterval(prevRow, nextRow);
 
-  // If the Interval Max is not 0:
-  if (interval.max) {
+  // If the Interval Max is 0, return original result.
+  if (!interval.max) {
+    return interval;
+  }
 
-    // Get the ID of the current row.
-    var id = jQuery(row).attr('id');
+  // Get the ID of the current row.
+  var id = jQuery(row).attr('id');
 
-    // Ensure that this is a Field.
-    var is_field = settings.row_id.hasOwnProperty(id);
+  // Get the Literal Next Row, even if it's being dragged with the current row.
+  var next = jQuery(row).next();
 
-    // Not a Text Field by Default.
-    var text_field = false;
+  // Get the Field Data for all Rows.
+  var row_data = Drupal.insertFieldFieldUI.row.getFieldData(row);
+  var prev_data = Drupal.insertFieldFieldUI.row.getFieldData(prevRow);
+  var next_data = Drupal.insertFieldFieldUI.row.getFieldData(next);
 
-    // If the Current Field is a Field:
-    if (is_field) {
+  // If the Curren Row is not a Field:
+  if (!row_data.is_field) {
 
-      // Set the Field Type to NULL.
-      var type;
+    // Prevent the Row from being a Child of a Field.
+    if (prev_data.is_parent || prev_data.has_parent) {
+      interval.max = interval.max - 1;
+      return interval;
+    }
+    else {
+      return interval;
+    }
 
-      // Get the Field Type from the Field Settings.
-      type = settings.field_settings[settings.row_id[id]].type;
+  }
 
-      // List of all posible Text types.
-      var types = [
-        'text_textarea',
-        'text_textarea_with_summary',
-        'text_default'
-      ];
+  // If the Previous Field has a parent and is a parent itself,
+  // then prevent fields from being inserted two levels deep.
+  if (prev_data.has_parent && prev_data.is_parent) {
+    interval.max = interval.max - 1;
+    return interval;
+  }
 
-      // If the field type is in the text field array, it is a text field.
-      if (jQuery.inArray(type, types)) {
-        text_field = true;
+  // By default, the Next Row is not a child of the Current Row.
+  var next_is_child = false;
+
+  // Only rows that are parents can have children.
+  if (row_data.is_parent) {
+
+    // If the Next Row has a parent, then it might be a child.
+    if (next_data.has_parent) {
+
+      // If the Next Row's parent, matches the current row, it is a child.
+      if (row_data.field_name == next_data.row_parent) {
+        next_is_child = true;
       }
 
     }
+  }
 
-    // The Field is not an Insert Field by Default.
-    var text_insert = false;
-
-    // Only Text Fields can be Insert Fields.
-    if (text_field) {
-
-      // Get the literal next row.
-      var next = jQuery(row).next('tr').get(0);
-
-      // If the indentation is greater of the next row than this row,
-      // then the current row has children, which makes it an insert field.
-      if (jQuery('.indentation', next).length > jQuery('.indentation', row).length) {
-        text_insert = true;
-      }
-
-    }
-
-    // Rather than basing the indent on the previous row,
-    // traverse the tree until a field that is not a leaf is found.
-    var prev = jQuery(prevRow);
-    var found = false;
-    var leaf = false;
-    var parent = false;
-
-    // Continue moving up the tree until a parent is found.
-    do {
-
-      // If the row is not a leaf, it can have children.
-      if (!jQuery(prev).hasClass('tabledrag-leaf')) {
-        found = true;
-
-        // Get the Parent (if there is one).
-        parent = jQuery('.field-parent.form-select', prev).get(0).value;
-      }
-      else {
-
-        // Continue to the previous row.
-        prev = jQuery(prev).prev('tr').get(0);
-
-      }
-
-    } while (prev && !found);
-
-    // If a suitable previous row was found:
-    if (found) {
-
-      // Modifications should only be performed on rows which are not
-      // Fields, are Insert Fields, or the Parent, has a Parent.
-      if (!is_field || text_insert || parent) {
-
-        // If this is not a Field and there is a Parent, set
-        // the literal parent as the previous field, to prevent
-        // non-fields from being falsely inserted.
-        if (!is_field && parent) {
-          prev = jQuery('#'+settings.field_settings[parent].row_id);
-        }
-
-        // Do not go deeper than as a child of the previous row.
-        var max = jQuery('.indentation', prev).length;
-
-        // Limit by the maximum allowed depth for the table.
-        interval.max = (max < interval.max) ? max : interval.max;
-
-      }
-
-    }
-
+  // Rows that already have children, should not be inserted into fields.
+  if (next_is_child && (prev_data.has_parent || prev_data.is_parent)) {
+    interval.max = interval.max - 1;
+    return interval;
   }
 
   // Return the interval.
   return interval;
 
 };
+
+/*
+ * Retreive Data about a Field.
+ */
+Drupal.insertFieldFieldUI.row.getFieldData = function(row) {
+
+  // Get the Insert Field Settings.
+  var settings = Drupal.settings.insert_field;
+
+  // Get the ID of the current row.
+  var id = jQuery(row).attr('id');
+
+  // Initialzie the Data object with defaults.
+  var data = {
+    is_field: false,
+    is_parent: false,
+    field_name: '',
+    row_parent: '',
+    has_parent: false
+  };
+
+  // Determine if the curren field is a field.
+  data.is_field = settings.row_id.hasOwnProperty(id);
+
+  // If the row is a field, it might be a parant, and
+  // it has a field name.
+  if (data.is_field) {
+
+    // Row is a parent if it's in the list of parents.
+    data.is_parent = settings.parents.hasOwnProperty(settings.row_id[id]);
+
+    // Get the Field Name.
+    data.field_name = settings.row_id[id];
+
+  }
+
+  // If the row has a Field Parent select box, it might have a parent.
+  if (jQuery('select.field-parent', row).length) {
+
+    // Get the value of the parent field.
+    data.row_parent = jQuery('select.field-parent', row).get(0).value;
+
+  }
+
+  // If the row is a Field and it has a Parent set,
+  // determine if it's has a field parent.
+  if (data.is_field && data.row_parent) {
+
+    // Field has a parent if the value of the select box, is in the list of parents.
+    data.has_parent = settings.parents.hasOwnProperty(data.row_parent);
+
+  }
+
+  // Return the modified data object.
+  return data;
+
+}
